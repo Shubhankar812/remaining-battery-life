d30ae1512c728463f9bb5033d2540eb81904e4f6
Shubhankar812 <shubhankarpande200@gmail.com>
2024-05-08 12:08:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import pickle
+from flask import Flask, request, jsonify
+import numpy as np
+import sklearn.metrics._dist_metrics as dist_metrics
+
+app = Flask(__name__)
+
+
+dt = pickle.load(open('dt_model.pkl', 'rb'))
+
+@app.route("/")
+def Home():
+    return 'Hello World!!'
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.get_json()
+    f1 = float(data['f1'])
+    f2 = float(data['f2'])
+    f3 = float(data['f3'])
+
+    final_input = np.array([f1, f2, f3]).reshape(1, -1)
+
+    output = dt.predict(final_input)
+
+    return jsonify(output[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
